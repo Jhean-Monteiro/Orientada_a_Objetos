c8b15ec8ed5b0a3508b3299eb39cd8bad9255f93
Jhean Monteiro <jheanmonteiro.2019@gmail.com>
2026-05-07 20:58:15 -0300

    Add files via upload

--- 07-05-26/app.js
+++ 07-05-26/app.js
@@ -10,9 +10,11 @@ class ContaCorrente extends ContaBase {
         try {
             if (this.saldo <= valor) {
                 throw new RangeError('CRASH.');
-            } else if (this.saldo > valor && valor == NaN)  {
-                this.saldo =- valor;
+            } else if (this.saldo > valor && valor != NaN)  {
+                this.saldo -= valor;
                 console.log(`${valor} sacado com sucesso. | Saldo Atual: ${this.saldo}`)
+            } else {
+                console.error("Tipo Inválido")
             }
 
         } catch (err) {
@@ -23,4 +25,4 @@ class ContaCorrente extends ContaBase {
 const user = new ContaCorrente(1000);
 
 console.log(user.saldo)
-user.sacar(2000)
+user.sacar(100)
